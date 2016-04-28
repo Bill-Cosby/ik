4b1bd440cabb6a38b2841455bdc3e40bdcea3dd2
Bill-Cosby <djhartman1216@gmail.com>
2016-04-28 11:56:39 -0700

    Visualized

--- main.cpp
+++ main.cpp
@@ -7,35 +7,46 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800,600), "IK");
     sf::Event event;
 
+    window.setFramerateLimit(60);
+
     const double pi = 3.14159;
     std::vector<double> test;
-    test.push_back(1);
-    test.push_back(0);
-    test.push_back(0);
+    test.push_back(20);
+    test.push_back(20);
+    test.push_back(20);
 
 
     vector<double> eulervector;
-    eulervector.push_back(0);
+    eulervector.push_back(.5);
     eulervector.push_back(1);
     eulervector.push_back(0);
 
-    while (window.isOpen()){
-    circ.setPosition(goal.x,goal.y);
+    sf::CircleShape circ;
+    circ.setFillColor(sf::Color::White);
 
-        vector<double> newPoint=NewPoint(test,pi/2,eulervector);
+    float counter = 0;
+    bool increasing = true;
+
+    while (window.isOpen()){
+        if (increasing)counter+=.01;
+        else counter-=.01;
+
+        if (counter == 2)increasing = 1-increasing;
+        else if (counter == 0)increasing = 1-increasing;
+        vector<double> newPoint=NewPoint(test,counter,eulervector);
 
         while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed){
                 window.close();
             }
         }
-
-
+        std::cout << newPoint[2] << std::endl;
+        circ.setRadius(test[2]+newPoint[2]);
+        circ.setPosition(newPoint[0]+1,newPoint[1]+1);
 
 
         window.clear();
         window.draw(circ);
-        me.update(window, goal);
         window.display();
 
     }
